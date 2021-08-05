362b0cbe47988cc7232561f3cff9a40503adc621
zoulfai <72633607+zoulfai@users.noreply.github.com>
2021-08-05 22:39:50 +0200

    fontion page produit

--- javascript/produit.js
+++ javascript/produit.js
@@ -1,17 +1,29 @@
 async function fetchText() {
     const queryString = window.location.search;
-    console.log(queryString);
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get('id')
-    console.log(id);
     let response = await fetch(`http://localhost:3000/api/teddies/${id}`);
     let ours = await response.json();
     let name = ours.name;
-    console.log(ours);
-    let elem = document.getElementById('oursName');
-    elem.innerHTML = `<h2 class="text-center">${name}</h2><br/>`;
+    let img = ours.imageUrl;
+    let description = ours.description ;
+    let prix = ours.price;
+    let elem0 = document.getElementById('oursName');
+    elem0.innerHTML = `<h2 class="text-center">${name}</h2>`;
+    let elem1 = document.getElementById('carte');
+    //carte rempli
+    elem1.innerHTML = `<div class="card" style="width:20rem;"> 
+      <img src="${img}" class="card-img-top" alt="ourson"> 
+        <div class="card-body text-center">
+          <h5 class="card-title">${description}</h5>
+          <p class="card-text"> ${prix}€</p>
+          <a href="produit.html?id=${id}" class="btn btn-primary">acheter</a>
+        </div>
+    </div>
+  </div>`
 }
 
 window.addEventListener('load', async function () {
   await fetchText();
 });
+
