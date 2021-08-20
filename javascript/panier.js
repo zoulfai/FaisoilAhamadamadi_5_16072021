8c25b32d1462cecbbac8bf4960c6c31d104f1a39
zoulfai <72633607+zoulfai@users.noreply.github.com>
2021-08-20 15:03:05 +0200

    modification panier

--- javascript/panier.js
+++ javascript/panier.js
@@ -1,3 +1,4 @@
+var oursTableau = [];
 async function fetchText(){
 
 
@@ -8,6 +9,7 @@ async function fetchText(){
     if (oursTmp){
       oursObject = JSON.parse(oursTmp);
     }
+    oursTableau = oursObject;
     let pannier = document.getElementById("oursName");
 
     for (let ours of oursObject) {
@@ -15,14 +17,16 @@ async function fetchText(){
         <img src="${ours.img}" width="50" height="50" alt="ourson" class="d-inline-block align-text-top">${ours.name} ${ours.prix}$</p>`;
     }
 
+//conversion du prix
    oursObject.forEach((ours)=> {
     convertitPrix = parseInt(ours.prix);
     totalPanier = totalPanier+ convertitPrix;
-
 });
+//calcul total panier
 const totalPrice = document.getElementById("total");
   totalPrice.innerHTML += `${totalPanier} $`;
 
+//affichage formulaire
   const form = document.getElementById("formulaire");
   const validation = document.getElementById("passCommande");
     const cacheButton = document.getElementById("cacheBouton");
@@ -30,22 +34,52 @@ const totalPrice = document.getElementById("total");
         form.classList.toggle("d-none");
         cacheButton.classList.add("d-none");
     });
+
+
 }
 
+ async function commande(){
+    alert ("commande");
+    const nom = document.getElementById("nom").value;
+    const prenom = document.getElementById("prenom").value;
+    const adresseMail = document.getElementById("adresseMail").value;
+    const codePostal = document.getElementById("codePostal").value;
+    const adresse = document.getElementById("adresse").value;
+    alert (nom,prenom,adresseMail,codePostal,adresse);
+    console.log(oursTableau);
+    let oursIds = oursTableau.map((ours)=>ours.id);
+    console.log(oursIds);
 
- /*async function commande(){
-    let oursTmp = localStorage.getItem('ours');
+    /*let oursTmp = localStorage.getItem('ours');
     console.log(oursTmp);
     let oursObject = [];
     if (oursTmp){
       oursObject = JSON.parse(oursTmp);
     }
-   await fetch("http://localhost:3000/api/teddies/order", {
-        method: "POST",
-        body: {products:oursObject,contact:{}}
+    console.log(oursObject);*/
+    try{
+       
+      let result = await fetch("http://localhost:3000/api/teddies/order", {
+        method: "POST", headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {products:oursIds,contact:{
+          firstName : "",
+          lastName : "",
+          address : "",
+          city : "",
+          email : ""
+          
+        }})
       })
+      console.log(result);
+    }
+    catch(error){console.log(error)}
 
-}*/
+   
+
+}
 
 
 window.addEventListener('load', async function() {
